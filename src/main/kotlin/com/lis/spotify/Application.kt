be0423e5193a190b33d15abce61640d6a114d1e3
Lis, Andrzej <andrzej.lis3@gmail.com>
2020-05-23 10:42:02 +0200

    fix scheduling

--- src/main/kotlin/com/lis/spotify/Application.kt
+++ src/main/kotlin/com/lis/spotify/Application.kt
@@ -15,13 +15,23 @@ package com.lis.spotify
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.scheduling.TaskScheduler
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 import org.springframework.web.socket.config.annotation.EnableWebSocket
+
 
 @SpringBootApplication
 @EnableWebSocket
+@EnableScheduling
 class Application {
     private val log = LoggerFactory.getLogger(Application::class.java)
+
+    @Bean
+    fun taskScheduler(): TaskScheduler {
+        return ThreadPoolTaskScheduler()
+    }
 }
 
 fun main(args: Array<String>) {
