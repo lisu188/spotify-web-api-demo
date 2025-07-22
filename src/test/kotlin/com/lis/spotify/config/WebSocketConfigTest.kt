cb5dd619b785ea18258c7c4c388b30efc8482349
lisu188 <andrzej.lis3@gmail.com>
2025-07-22 11:53:08 +0200

    Handle Last.fm auth error (#42)

--- src/test/kotlin/com/lis/spotify/config/WebSocketConfigTest.kt
+++ src/test/kotlin/com/lis/spotify/config/WebSocketConfigTest.kt
@@ -2,6 +2,8 @@ package com.lis.spotify.config
 
 import io.mockk.every
 import io.mockk.mockk
+import jakarta.websocket.server.HandshakeRequest
+import jakarta.websocket.server.ServerEndpointConfig
 import org.junit.jupiter.api.Assertions.assertSame
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -29,5 +31,32 @@ class WebSocketConfigTest {
     configurator.setApplicationContext(ctx)
     val result = configurator.getEndpointInstance(Dummy::class.java)
     assertSame(bean, result)
+  }
+
+  class DummyRequest(val request: jakarta.servlet.http.HttpServletRequest) : HandshakeRequest {
+    override fun getHeaders(): MutableMap<String, MutableList<String>> = mutableMapOf()
+
+    override fun getParameterMap(): MutableMap<String, MutableList<String>> = mutableMapOf()
+
+    override fun getQueryString(): String? = null
+
+    override fun getRequestURI(): java.net.URI? = null
+
+    override fun getUserPrincipal(): java.security.Principal? = null
+
+    override fun isUserInRole(role: String?): Boolean = false
+
+    override fun getHttpSession(): Any? = null
+  }
+
+  @Test
+  fun modifyHandshakeExtractsClientId() {
+    val httpReq = mockk<jakarta.servlet.http.HttpServletRequest>()
+    every { httpReq.cookies } returns arrayOf(jakarta.servlet.http.Cookie("clientId", "cid"))
+    val req = DummyRequest(httpReq)
+    val config = ServerEndpointConfig.Builder.create(Dummy::class.java, "/").build()
+    val configurator = WebsocketSpringConfigurator()
+    configurator.modifyHandshake(config, req, mockk())
+    assertSame("cid", config.userProperties["clientId"])
   }
 }
